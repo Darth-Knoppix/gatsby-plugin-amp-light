cb804e45e21d2eff50398c7c481b10bc7396d951
Seth Corker <seth.corker@gmail.com>
2020-12-05 09:20:39 +0000

    fix: 🐛 transform plugin correctly to work with React

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
   plugins: [
     "@babel/plugin-syntax-jsx",
     "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-runtime",
   ],
 };
